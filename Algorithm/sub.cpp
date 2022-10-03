e754cb63e6e88e7a2fbad04115eb29f319474a32
kle-wang <mckaile@outlook.com>
2022-10-03 15:23:48 +0800

    高精度加法

--- Algorithm/sub.cpp
+++ Algorithm/sub.cpp
@@ -1,6 +1,9 @@
+//高精度减法
 #include <iostream>
 #include <vector>
 using namespace std;
+
+//比较是否A >= B
 bool cmp(vector <int> &A,vector <int> &B){
     if (A.size() != B.size()) return A.size() > B.size();
     for(int i = A.size() - 1; i >= 0; i--){
@@ -11,7 +14,7 @@ bool cmp(vector <int> &A,vector <int> &B){
     return true;
 }
 
-// c = a - b
+// 进行 C = A - B
 vector<int> sub(vector <int> &A,vector <int> &B){
     vector<int> C;
     for(int i = 0, t = 0; i < A.size(); i++){
@@ -30,17 +33,19 @@ int main(){
     string a,b;
     vector<int>A,B;
     cin>>a>>b;
+    //字符串转到数组里 倒序插入
     for(int i = a.size() - 1; i >= 0; i--) A.push_back(a[i] - '0');
     for(int i = b.size() - 1; i >= 0; i--) B.push_back(b[i] - '0');
+
+    //比大小
     if(cmp(A,B)){
+        // A>=B
         auto C = sub(A,B);
         for(int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]);
     }else{
+        //A<B
         auto C = sub(B,A);
         cout << "-";
         for(int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]);
     }
-
-
-
 }
